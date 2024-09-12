e85a9623d261756f46fb64d77e3daba779cf7bbb
yuens <yuens.me@gmail.com>
2024-09-11 21:30:41 -0400

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 import prisma from '@/db/prismaDb';
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        {users?.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </main>
-  );
+  return 'home';
 }
