db9b1a682529ed8fee416a0388fbbe41a37acad1
yuens <yuens.me@gmail.com>
2024-09-15 17:10:57 -0400

    update db schema

---
+++ lib/types.ts
@@ -0,0 +1,11 @@
+export type dummyPost = {
+  title: string;
+  reactions: {
+    likes: string;
+    dislikes: string;
+  };
+  body: string;
+  tags: string[];
+  views: number;
+  categories: string[];
+};
